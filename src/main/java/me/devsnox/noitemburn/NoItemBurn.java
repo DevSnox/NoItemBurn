802aaf29757577b2e0682e134fa5b5ddd64e9fe0
DevSnox <me.devsnox@gmail.com>
2019-02-11 22:45:18 +0100

    Add ground-structure for a Spigot plugin #4

---
+++ src/main/java/me/devsnox/noitemburn/NoItemBurn.java
@@ -0,0 +1,19 @@
+package me.devsnox.noitemburn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by DevSnox on 12.02.18
+ * Copyright (c) 2018 DevSnox
+ * GitHub: https://github.com/DevSnox
+ * Web: http://devsnox.me
+ * Mail: me.devsnox@gmail.com
+ * Discord: DevSnox#4884 | Skype: live:chaos3729
+ */
+public class NoItemBurn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
