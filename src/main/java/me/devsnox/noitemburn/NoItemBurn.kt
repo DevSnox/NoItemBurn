fb9643648d47d7e7ae6b0fdf52e18f44e019fcf0
Yasin Dalal <yasin@dalal.ch>
2019-12-06 00:34:53 +0100

    Added permission for NoItemBurn #14

--- src/main/java/me/devsnox/noitemburn/NoItemBurn.kt
+++ src/main/java/me/devsnox/noitemburn/NoItemBurn.kt
@@ -1,12 +1,15 @@
 package me.devsnox.noitemburn
 
-import net.minecraft.server.v1_8_R3.Entity
+import org.bukkit.Material
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity
+import org.bukkit.entity.Entity
 import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.entity.EntitySpawnEvent
+import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
@@ -19,16 +22,36 @@ import org.bukkit.plugin.java.JavaPlugin
  */
 class NoItemBurn : JavaPlugin(), Listener {
 
+    private val permission = "noitemburn.use"
+
     override fun onEnable() {
         this.server.pluginManager.registerEvents(this, this)
     }
 
     @EventHandler(priority = EventPriority.LOW)
-    fun onDrop(event: EntitySpawnEvent) {
-        if (event.entity.type == EntityType.DROPPED_ITEM) {
-            val field = Entity::class.java.getDeclaredField("fireProof")
-            field.isAccessible = true
-            field.setBoolean((event.entity as CraftEntity).handle, true)
+    fun onBreak(event: BlockBreakEvent) {
+        if(event.player.hasPermission(this.permission)) {
+            val block = event.block
+            val drops = block.drops
+
+            block.type = Material.AIR
+            drops.forEach { this.fireProtection(block.world.dropItem(block.location, it)) }
         }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    fun onDrop(event: PlayerDropItemEvent) {
+        if(event.player.hasPermission(this.permission)) {
+            this.fireProtection(event.itemDrop)
+        }
+    }
+
+    private fun fireProtection(entity: Entity) : Entity {
+        val field = Entity::class.java.getDeclaredField("fireProof")
+
+        field.isAccessible = true
+        field.setBoolean((entity as CraftEntity).handle, true)
+
+        return entity
     }
 }
