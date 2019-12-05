83ff15d348c04a9f7e98243a16fa0e896fb9aeb3
Yasin Dalal <yasin@dalal.ch>
2019-12-06 00:47:25 +0100

    Updated contact informations #23

--- src/main/java/me/devsnox/noitemburn/NoItemBurn.kt
+++ src/main/java/me/devsnox/noitemburn/NoItemBurn.kt
@@ -11,12 +11,10 @@ import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
- * Created by DevSnox on 12.02.18
- * Copyright (c) 2018 DevSnox
+ * Created by Yasin Dalal on 12.02.18
+ * Copyright (c) 2018 Yasin Dalal
  * GitHub: https://github.com/DevSnox
- * Web: http://devsnox.me
- * Mail: me.devsnox@gmail.com
- * Discord: DevSnox#4884 | Skype: live:chaos3729
+ * E-Mail: yasin@dalal.ch
  */
 class NoItemBurn : JavaPlugin(), Listener {
 
